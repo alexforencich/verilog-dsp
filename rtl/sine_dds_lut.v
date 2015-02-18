818c51cd9b560de65cc69cbba05d6f88ae938252
Alex Forencich <alex@alexforencich.com>
2015-02-17 22:47:40 -0800

    Add sine DDS LUT module

---
+++ rtl/sine_dds_lut.v
@@ -0,0 +1,149 @@
+/*
+
+Copyright (c) 2015 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+// Language: Verilog 2001
+
+`timescale 1ns / 1ps
+
+/*
+ * Sine DDS look up table
+ */
+module sine_dds_lut #
+(
+    parameter OUTPUT_WIDTH = 16,
+    parameter INPUT_WIDTH = OUTPUT_WIDTH+2
+)
+(
+    input  wire                    clk,
+    input  wire                    rst,
+
+    input  wire [INPUT_WIDTH-1:0]  input_phase_tdata,
+    input  wire                    input_phase_tvalid,
+    output wire                    input_phase_tready,
+
+    output wire [OUTPUT_WIDTH-1:0] output_sample_i_tdata,
+    output wire [OUTPUT_WIDTH-1:0] output_sample_q_tdata,
+    output wire                    output_sample_tvalid,
+    input  wire                    output_sample_tready
+);
+
+localparam W = (INPUT_WIDTH-2)/2;
+
+reg [INPUT_WIDTH-1:0] phase_reg = 0;
+
+integer i;
+
+// coarse sine and cosine LUTs
+reg [OUTPUT_WIDTH-1:0] coarse_c_lut[2**(W+1)-1:0];
+reg [OUTPUT_WIDTH-1:0] coarse_s_lut[2**(W+1)-1:0];
+
+initial begin
+    for (i = 0; i < 2**(W+1); i = i + 1) begin
+        coarse_c_lut[i] = $cos(2*3.1415926535*i/2**(W+2))*(2**(OUTPUT_WIDTH-1)-1);
+        coarse_s_lut[i] = $sin(2*3.1415926535*i/2**(W+2))*(2**(OUTPUT_WIDTH-1)-1);
+    end
+end
+
+// fine sine LUT
+reg [(OUTPUT_WIDTH/2)-1:0] fine_s_lut[2**W-1:0];
+
+initial begin
+    for (i = 0; i < 2**W; i = i + 1) begin
+        fine_s_lut[i] = $sin(2*3.1415926535*(i-2**(W-1))/2**INPUT_WIDTH)*(2**(OUTPUT_WIDTH-1)-1);
+    end
+end
+
+reg [OUTPUT_WIDTH-1:0] sample_i_reg = 0;
+reg [OUTPUT_WIDTH-1:0] sample_q_reg = 0;
+
+wire SIGN = phase_reg[INPUT_WIDTH-1];
+wire SLOPE = phase_reg[INPUT_WIDTH-2];
+wire [W:0] A = phase_reg[INPUT_WIDTH-2:W];
+wire [W-1:0] B = phase_reg[W-1:0];
+
+reg sign_reg_1 = 0;
+reg sign_reg_2 = 0;
+reg sign_reg_3 = 0;
+reg sign_reg_4 = 0;
+
+reg [OUTPUT_WIDTH-1:0] ccs_reg_1 = 0, ccs_reg_2 = 0, ccs_reg_3 = 0;
+reg [OUTPUT_WIDTH-1:0] css_reg_1 = 0, css_reg_2 = 0, css_reg_3 = 0;
+reg [(OUTPUT_WIDTH/2)-1:0] fss_reg_1 = 0, fss_reg_2 = 0;
+
+reg [(OUTPUT_WIDTH*2)-1:0] cp_reg_1 = 0;
+reg [(OUTPUT_WIDTH*2)-1:0] sp_reg_1 = 0;
+
+reg [OUTPUT_WIDTH-1:0] cs_reg_1 = 0;
+reg [OUTPUT_WIDTH-1:0] ss_reg_1 = 0;
+
+assign input_phase_tready = output_sample_tready;
+assign output_sample_i_tdata = sample_i_reg;
+assign output_sample_q_tdata = sample_q_reg;
+assign output_sample_tvalid = input_phase_tvalid;
+
+always @(posedge clk) begin
+    if (rst) begin
+        phase_reg <= 0;
+    end else begin
+        if (input_phase_tready & input_phase_tvalid) begin
+            phase_reg <= input_phase_tdata;
+        end
+    end
+
+    if (input_phase_tready & input_phase_tvalid) begin
+        // pipeline sits primarily in DSP slice
+        // sin(A+B) = sin(A) + cos(A)*sin(B)
+        // cos(A+B) = cos(A) - sin(A)*sin(B)
+        
+        // read samples
+        sign_reg_1 <= SIGN;
+        ccs_reg_1 <= coarse_c_lut[A];
+        css_reg_1 <= coarse_s_lut[A];
+        fss_reg_1 <= fine_s_lut[B];
+
+        // pipeline
+        sign_reg_2 <= sign_reg_1;
+        ccs_reg_2 <= ccs_reg_1;
+        css_reg_2 <= css_reg_1;
+        fss_reg_2 <= fss_reg_1;
+
+        // multiply
+        sign_reg_3 <= sign_reg_2;
+        ccs_reg_3 <= ccs_reg_2;
+        css_reg_3 <= css_reg_2;
+        cp_reg_1 <= $signed(css_reg_2) * $signed(fss_reg_2);
+        sp_reg_1 <= $signed(ccs_reg_2) * $signed(fss_reg_2);
+
+        // add
+        sign_reg_4 <= sign_reg_3;
+        cs_reg_1 <= ccs_reg_3 - (cp_reg_1 >> (OUTPUT_WIDTH-1));
+        ss_reg_1 <= css_reg_3 + (sp_reg_1 >> (OUTPUT_WIDTH-1));
+
+        // negate output samples
+        sample_i_reg <= sign_reg_4 ? -cs_reg_1 : cs_reg_1;
+        sample_q_reg <= sign_reg_4 ? -ss_reg_1 : ss_reg_1;
+    end
+end
+
+endmodule
---
+++ tb/test_sine_dds_lut.py
@@ -0,0 +1,248 @@
+#!/usr/bin/env python2
+"""
+
+Copyright (c) 2015 Alex Forencich
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+"""
+
+from myhdl import *
+import os
+from Queue import Queue
+import numpy as np
+
+import axis_ep
+
+module = 'sine_dds_lut'
+
+srcs = []
+
+srcs.append("../rtl/%s.v" % module)
+srcs.append("test_%s.v" % module)
+
+src = ' '.join(srcs)
+
+build_cmd = "iverilog -o test_%s.vvp %s" % (module, src)
+
+def dut_sine_dds_lut(clk,
+                     rst,
+                     current_test,
+
+                     input_phase_tdata,
+                     input_phase_tvalid,
+                     input_phase_tready,
+
+                     output_sample_i_tdata,
+                     output_sample_q_tdata,
+                     output_sample_tvalid,
+                     output_sample_tready):
+
+    if os.system(build_cmd):
+        raise Exception("Error running build command")
+    return Cosimulation("vvp -m myhdl test_%s.vvp -lxt2" % module,
+                clk=clk,
+                rst=rst,
+                current_test=current_test,
+
+                input_phase_tdata=input_phase_tdata,
+                input_phase_tvalid=input_phase_tvalid,
+                input_phase_tready=input_phase_tready,
+
+                output_sample_i_tdata=output_sample_i_tdata,
+                output_sample_q_tdata=output_sample_q_tdata,
+                output_sample_tvalid=output_sample_tvalid,
+                output_sample_tready=output_sample_tready)
+
+def bench():
+
+    # Parameters
+    OUTPUT_WIDTH = 16
+    INPUT_WIDTH = OUTPUT_WIDTH+2
+
+    # Inputs
+    clk = Signal(bool(0))
+    rst = Signal(bool(0))
+    current_test = Signal(intbv(0)[8:])
+
+    input_phase_tdata = Signal(intbv(0)[INPUT_WIDTH:])
+    input_phase_tvalid = Signal(bool(0))
+    output_sample_tready = Signal(bool(0))
+
+    # Outputs
+    input_phase_tready = Signal(bool(0))
+    output_sample_i_tdata = Signal(intbv(0)[OUTPUT_WIDTH:])
+    output_sample_q_tdata = Signal(intbv(0)[OUTPUT_WIDTH:])
+    output_sample_tvalid = Signal(bool(0))
+
+    # sources and sinks
+    phase_source_queue = Queue()
+    phase_source_pause = Signal(bool(0))
+    sample_sink_queue = Queue()
+    sample_sink_pause = Signal(bool(0))
+
+    phase_source = axis_ep.AXIStreamSource(clk,
+                                           rst,
+                                           tdata=input_phase_tdata,
+                                           tvalid=input_phase_tvalid,
+                                           tready=input_phase_tready,
+                                           fifo=phase_source_queue,
+                                           pause=phase_source_pause,
+                                           name='phase_source')
+
+    sample_sink = axis_ep.AXIStreamSink(clk,
+                                        rst,
+                                        tdata=ConcatSignal(output_sample_q_tdata, output_sample_i_tdata),
+                                        tvalid=output_sample_tvalid,
+                                        tready=output_sample_tready,
+                                        tkeep=Signal(intbv(3)[2:]),
+                                        fifo=sample_sink_queue,
+                                        pause=sample_sink_pause,
+                                        name='sample_sink')
+
+    # DUT
+    dut = dut_sine_dds_lut(clk,
+                           rst,
+                           current_test,
+
+                           input_phase_tdata,
+                           input_phase_tvalid,
+                           input_phase_tready,
+
+                           output_sample_i_tdata,
+                           output_sample_q_tdata,
+                           output_sample_tvalid,
+                           output_sample_tready)
+
+    @always(delay(4))
+    def clkgen():
+        clk.next = not clk
+
+    @instance
+    def check():
+        yield delay(100)
+        yield clk.posedge
+        rst.next = 1
+        yield clk.posedge
+        rst.next = 0
+        yield clk.posedge
+        yield delay(100)
+        yield clk.posedge
+
+        # testbench stimulus
+
+        yield clk.posedge
+        print("test 1: coarse check")
+        current_test.next = 1
+
+        # check single cycle
+        test_frame = axis_ep.AXIStreamFrame()
+        test_frame.data = list(range(0,2**18,2**10)) + [0]*6
+
+        phase_source_queue.put(test_frame)
+
+        yield clk.posedge
+        yield clk.posedge
+
+        while not phase_source_queue.empty() or input_phase_tvalid:
+            yield clk.posedge
+
+        yield clk.posedge
+        yield clk.posedge
+
+        # pipeline delay
+        for j in range(6):
+            sample_sink_queue.get(False)
+
+        for j in range(len(test_frame.data)-6):
+            rx_frame = sample_sink_queue.get(False)
+            x = test_frame.data[j]
+            c = rx_frame.data[0]
+            s = rx_frame.data[1]
+
+            # sign bit
+            if c >= 2**(OUTPUT_WIDTH-1):
+                c -= 2**OUTPUT_WIDTH
+            if s >= 2**(OUTPUT_WIDTH-1):
+                s -= 2**OUTPUT_WIDTH
+            
+            # reference sine and cosine
+            rc = int(np.cos(2*np.pi*(x-2**((INPUT_WIDTH-2)/2-1))/2**INPUT_WIDTH)*(2**(OUTPUT_WIDTH-1)-1))
+            rs = int(np.sin(2*np.pi*(x-2**((INPUT_WIDTH-2)/2-1))/2**INPUT_WIDTH)*(2**(OUTPUT_WIDTH-1)-1))
+
+            # assert error of two counts or less
+            assert abs(c-rc) <= 2
+            assert abs(s-rs) <= 2
+
+        yield clk.posedge
+        print("test 2: fine check")
+        current_test.next = 2
+
+        # check two zero crossing regions
+        test_frame = axis_ep.AXIStreamFrame()
+        test_frame.data = list(range(2**18-2**8,2**18-1)) + list(range(0,2**8)) + list(range(2**16-2**8, 2**16+2**8)) + [0]*6
+
+        phase_source_queue.put(test_frame)
+
+        yield clk.posedge
+        yield clk.posedge
+
+        while not phase_source_queue.empty() or input_phase_tvalid:
+            yield clk.posedge
+
+        yield clk.posedge
+        yield clk.posedge
+
+        # pipeline delay
+        for j in range(6):
+            sample_sink_queue.get(False)
+
+        for j in range(len(test_frame.data)-6):
+            rx_frame = sample_sink_queue.get(False)
+            x = test_frame.data[j]
+            c = rx_frame.data[0]
+            s = rx_frame.data[1]
+
+            # sign bit
+            if c >= 2**(OUTPUT_WIDTH-1):
+                c -= 2**OUTPUT_WIDTH
+            if s >= 2**(OUTPUT_WIDTH-1):
+                s -= 2**OUTPUT_WIDTH
+            
+            # reference sine and cosine
+            rc = int(np.cos(2*np.pi*(x-2**((INPUT_WIDTH-2)/2-1))/2**INPUT_WIDTH)*(2**(OUTPUT_WIDTH-1)-1))
+            rs = int(np.sin(2*np.pi*(x-2**((INPUT_WIDTH-2)/2-1))/2**INPUT_WIDTH)*(2**(OUTPUT_WIDTH-1)-1))
+
+            # assert error of two counts or less
+            assert abs(c-rc) <= 2
+            assert abs(s-rs) <= 2
+
+        yield delay(100)
+
+        raise StopSimulation
+
+    return dut, phase_source, sample_sink, clkgen, check
+
+def test_bench():
+    sim = Simulation(bench())
+    sim.run()
+
+if __name__ == '__main__':
+    print("Running test...")
+    test_bench()
